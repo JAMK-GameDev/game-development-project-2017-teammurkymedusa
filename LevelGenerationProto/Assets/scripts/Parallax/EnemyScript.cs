aa42ab9802e4dca2a3a04e23ab686546fb7436a1
Eurybus <wraitholyth@gmail.com>
2017-04-03 15:15:42 +0300

    Parallax scrolling works now

---
+++ LevelGenerationProto/Assets/scripts/Parallax/EnemyScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    private bool hasSpawn;
+
+    private Collider2D colliderComponent;
+
+    private SpriteRenderer rendererComponent;
+
+    private void Awake()
+    {
+
+        colliderComponent = GetComponent<Collider2D>();
+        rendererComponent = GetComponent<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+	void Start ()
+	{
+
+	    hasSpawn = false;
+	    colliderComponent.enabled = false;
+	    rendererComponent.enabled = false;
+	    //Disable other scripts enemy object might have
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!hasSpawn)
+	    {
+	        if (rendererComponent.IsVisibleFrom(Camera.main))
+	        {
+	            Spawn();
+	        }
+	    }
+	    else
+	    {
+	        //Is object out of camera? If so, we destroy it
+	        if (!rendererComponent.IsVisibleFrom(Camera.main))
+	        {
+	            Debug.Log("Destroying " + gameObject.name);
+	            Destroy(gameObject);
+	        }
+	    }
+	}
+
+    private void Spawn()
+    {
+        //Debug.Log(gameObject.name + " Spawned");
+        colliderComponent.enabled = true;
+        rendererComponent.enabled = true;
+
+        //Enable other scripts and components
+    }
+}
