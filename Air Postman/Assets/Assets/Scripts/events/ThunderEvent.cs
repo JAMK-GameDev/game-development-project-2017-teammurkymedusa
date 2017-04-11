64741334ae969a5f61097dfcde71f2a1f2d0f6cd
Matias Kivikoura <matias.kivikoura@gmail.com>
2017-04-11 15:46:28 +0300

    Add thunderflash

--- Air Postman/Assets/Assets/Scripts/events/ThunderEvent.cs
+++ Air Postman/Assets/Assets/Scripts/events/ThunderEvent.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class ThunderEvent : MonoBehaviour
 {
-    public float EventSpawnPoint;
-    public float EventDespawnPoint;
-    public GameObject ThunderFlashObject;
+    private BoxCollider2D collisionArea;
 
 	void Start()
     {
-		
-	}
-	
-	void Update()
-    {
-		
+        collisionArea = GetComponent<BoxCollider2D>();
+        collisionArea.size = new Vector2(100.0f, Camera.main.orthographicSize * 2.0f);
 	}
 }
