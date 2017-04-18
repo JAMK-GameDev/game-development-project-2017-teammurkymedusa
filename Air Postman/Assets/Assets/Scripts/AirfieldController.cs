b512d96e0d6033608cc472c1c1b91845a9c96207
RedPanduzer <r.k.n.post@gmail.com>
2017-04-18 13:44:03 +0300

    Map view, level selection, plane sprite and started work on skybox and ground death

---
+++ Air Postman/Assets/Assets/Scripts/AirfieldController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class AirfieldController : MonoBehaviour
+    {
+        public GameObject PlayMenu;
+        public Button Airfield;
+        public string LevelName;
+        public float SelectorPosition_X;
+        public float SelectorPosition_Y;
+
+        private void Start()
+        {
+            Airfield.GetComponent<Button>().onClick.AddListener(ActivatePlayMenu);
+        }
+
+        void ActivatePlayMenu()
+        {
+            PlayMenu.SetActive(true);
+            //set play menu to proper location
+            PlayMenu.transform.position =  new Vector3(SelectorPosition_X, SelectorPosition_Y, 0);
+            PlayMenu.GetComponent<PlayButton>().SetLevelName(LevelName);
+        }
+    }
+}
---
+++ Air Postman/Assets/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class PlayButton : MonoBehaviour
+    {
+        public Button PlaySprite;
+
+        private string level;
+        private bool _levelValid = false;
+
+        private void Start()
+        {
+            PlaySprite.GetComponent<Button>().onClick.AddListener(StartGame);
+        }
+
+        public void SetLevelName(string name)
+        {
+            Scene level = SceneManager.GetSceneByName(name);
+            //if (level.IsValid())
+            //{
+                this.level = name;
+                _levelValid = true;
+            //}
+            //else
+            //{
+                //Debug.LogWarning("Invalid Scene name given, name was " + name);
+                //_levelValid = false;
+            //}
+        }
+
+        void StartGame()
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
