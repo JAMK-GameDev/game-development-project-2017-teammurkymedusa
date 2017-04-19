8f7dd4b0314d4627534e626b1f6928b08ed47ca6
Matias Kivikoura <matias.kivikoura@gmail.com>
2017-04-19 15:14:06 +0300

    Add graphics and initial script

---
+++ Air Postman/Assets/Assets/Scripts/UI/SpeedIndicator.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedIndicator : MonoBehaviour
+{
+    public GameObject Aircraft;
+    public GameObject SpeedIndicatorNeedle;
+    private Vector3 initialRotation;
+    private float speed;
+
+	void Start()
+    {
+        Aircraft = Aircraft ?? GameObject.FindGameObjectWithTag("Player");
+        SpeedIndicatorNeedle = SpeedIndicatorNeedle ?? GameObject.Find("SpeedIndicatorNeedle");
+        initialRotation = transform.eulerAngles;
+	}
+	
+	void FixedUpdate()
+    {
+        speed = Aircraft.GetComponent<PlaneControls>().GetSpeed();
+        Vector3 needleRotation = new Vector3(0.0f, 0.0f, speed - initialRotation.z);
+        SpeedIndicatorNeedle.transform.localEulerAngles = needleRotation;
+	}
+}
